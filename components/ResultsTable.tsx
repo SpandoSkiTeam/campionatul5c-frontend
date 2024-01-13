02f96d9bf230bc50bbc583ac2477d096aada18a1
Andrei Tudorica <andreitudoricacj@gmail.com>
2024-01-13 13:57:45 +0200

    fix time formatting

--- components/ResultsTable.tsx
+++ components/ResultsTable.tsx
@@ -118,7 +118,10 @@ const getTotalTime = (time1, time2) => {
   const seconds = totalSeconds % 60;
   const milliseconds = totalMilliseconds % 1000;
 
-  return `${minutes}:${seconds}.${milliseconds.toString().padStart(3, "0")}`;
+  // return `${minutes}:${seconds}.${milliseconds.toString().padStart(3, "0")}`;
+  return `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}.${milliseconds.toString().padStart(3, "0")}`;
 };
 
 const processRows = (runs) => {
