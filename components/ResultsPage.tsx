c57f6aa7d2c6d9d6d894a6581b50bdb010477daa
AndreiSpando <andrei.tudorica@spandoskiteam.ro>
2025-02-02 17:30:20 +0200

    set race to 0

--- components/ResultsPage.tsx
+++ components/ResultsPage.tsx
@@ -84,7 +84,7 @@ const ResultsPage: React.FC = () => {
   const [searchFilter, setSearchFilter] = useState("");
   const [races, setRaces] = useState<any>([]);
   const [selectedAgeGroup, setSelectedAgeGroup] = useState("");
-  const currentRace = 1;
+  const currentRace = 0;
   const [selectedRace, setSetSelectedRace] = useState(currentRace);
   const [displayChampionshipResults, setDisplayChampionshipResults] =
     useState(false);
